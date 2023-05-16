6e3fbf66dd8cb67c7d15557b11d3bd32ddbaad7e
racitifilippo <92914437+racitifilippo@users.noreply.github.com>
2023-05-16 12:14:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class MySite(object):
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def index(self):
-        cherrypy.response.headers['Access-Control-Allow-Origin'] = 'https://4200-racitifilip-960esercita-fo343xmn9v5.ws-eu97.gitpod.io'
+        cherrypy.response.headers['Access-Control-Allow-Origin'] = '*'
         cherrypy.response.headers['Access-Control-Allow-Headers'] = 'ngrok-skip-browser-warning'    
         #cherrypy.response.headers['Access-Control-Allow-Methods']=  "POST, GET, PUT, DELETE, OPTIONS"
         #cherrypy.response.headers['Access-Control-Allow-Credentials']=  "true"
